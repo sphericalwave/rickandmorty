2480f862b3ae4db062738bd1ced6504b5a3424c7
aaronium112 <aaron@sphericalwave.com>
2022-12-11 22:23:45 -0400

    async imgs

---
+++ RickAndMorty/RickAndMorty/CharactersUi/AvatarUi.swift
@@ -0,0 +1,20 @@
+//
+//  AvatarUi.swift
+//  RickAndMorty
+//
+//  Created by Aaron Anthony on 2022-12-11.
+//
+
+import SwiftUI
+
+struct AvatarUi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AvatarUi_Previews: PreviewProvider {
+    static var previews: some View {
+        AvatarUi()
+    }
+}
